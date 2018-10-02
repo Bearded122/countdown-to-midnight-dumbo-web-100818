426a2a2d0bf154b796c4e388e24f02d90f7aa8ae
John Sime <johnc.sime@gmail.com>
2018-10-02 04:37:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,8 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    break if number == 0
-    puts "HAPPY NEW YEAR!"
   end
+  puts "HAPPY NEW YEAR!"
 end
 
 
