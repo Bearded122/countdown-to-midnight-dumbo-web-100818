b72b07bb367417c5aa69e93aba5331d13441a82e
John Sime <johnc.sime@gmail.com>
2018-10-02 04:23:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,16 @@
 
 def countdown
   number = 10
-  while number > 10
+  while number > 11
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+end
+
+
+def countdown_with_sleep
+  number = 10
+  while number > 11
     puts "#{number} SECOND(S)!"
     number -= 1
   end
