f9c96941106d29f11f82b59527e85b694d91c3de
John Sime <johnc.sime@gmail.com>
2018-10-02 04:33:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
 #write your code here
 
-def countdown(sec)
-  for sec in 0..10
-    puts "#{sec} SECOND(S)!"
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
